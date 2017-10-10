4e0e7176cdadbfaff2a5c70e31afe2d6ac3ad8d7
Alexandr <ar.kraushkin@gmail.com>
2017-10-10 08:15:33 +0300

    new commi2t

---
+++ Desktop/git_exercise/test.py
@@ -0,0 +1,9 @@
+number=42
+while True:
+
+    otvet=input("Вводи сука код: ")
+    if otvet==number: print("break")
+
+
+
+
---
+++ Desktop/git_exercise/test2.py
@@ -0,0 +1,11 @@
+number=42
+while True:
+
+    werwerewrew
+
+    otvet=input("Вводи сука код: ")
+    if otvet==number: print("break")
+
+
+
+
